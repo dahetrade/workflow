c5b996fb894008079bfd03d4f7b190fb67e3e887
dahetrade <dahetrade@sina.com>
2020-07-02 18:25:40 +0800

    test

---
+++ src/main/java/com/xiek/demoactiviti/DemoActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.xiek.demoactiviti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoActivitiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiek/demoactiviti/DemoActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiek.demoactiviti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
